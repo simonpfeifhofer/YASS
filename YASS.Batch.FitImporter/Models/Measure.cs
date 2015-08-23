4411b212e8794cc1a4555d7ca5d3453fb3f8e92d
simon.pfeifhofer <simon.pfeifhofer@world-direct.at>
2015-08-23 16:04:23 +0200

    * add tcx file importer

---
+++ YASS.Batch.FitImporter/Models/Measure.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YASS.Batch.FitImporter.Models
+{
+    public class Measure
+    {
+
+        public DateTimeOffset Timestamp { get; set; }
+        public string ActivityIdentifier { get; set; }
+        public string GroupIdentifier { get; set; }
+        public string EntityIdentifier { get; set; }
+        public string SensorName { get; set; }
+        public SensorType SensorType { get; set; }
+        public string Value { get; set; }
+
+    }
+}
---
+++ YASS.Batch.FitImporter/Program.cs
@@ -0,0 +1,87 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using YASS.Batch.FitImporter.Models;
+
+namespace YASS.Batch.FitImporter
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            try { 
+
+            string endpoint = args[0];
+            string file = args[1];
+
+            var serializer = new XmlSerializer(typeof(TrainingCenterDatabase_t));
+
+            TrainingCenterDatabase_t database = null;
+
+            using (var sr = new StreamReader(file))
+            {
+                database = serializer.Deserialize(sr) as TrainingCenterDatabase_t;
+            }
+
+            if(database.Activities == null || database.Activities.Activity == null)
+            {
+                return;
+            }
+
+            foreach(var ac in database.Activities.Activity)
+            {
+                foreach(var lap in ac.Lap)
+                {
+                    foreach(var tp in lap.Track)
+                    {
+                        SubmitTrackPoint(ac, lap, tp, endpoint);
+                        Console.WriteLine("Trackpoint {0} submitted!", tp.Time);
+                    }
+                }
+            }
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+        }
+
+        private static void SubmitTrackPoint(Activity_t activity, ActivityLap_t lap, Trackpoint_t tp, string url)
+        {
+
+            string requestString = JsonConvert.SerializeObject(
+                new Measure()
+                {
+                    ActivityIdentifier = activity.Id.ToString(),
+                    GroupIdentifier = "Team Dowe Sportswear",
+                    Timestamp = tp.Time,
+                    EntityIdentifier = "Simon Pfeifhofer",
+                    SensorName = "HeartRate",
+                    SensorType = SensorType.Numeric,
+                    Value = Convert.ToDouble(tp.HeartRateBpm.Value).ToString()
+                }
+            );
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            request.ContentLength = requestString.Length;
+            using (StreamWriter requestWriter = new StreamWriter(request.GetRequestStream()))
+            {
+                requestWriter.Write(requestString);
+            }
+
+        }
+
+    }
+}
